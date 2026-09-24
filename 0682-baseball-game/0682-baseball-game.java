aca06083f29366afac597e62c6e068bced87ba96
rahulsharma2910-star <229693313+rahulsharma2910-star@users.noreply.github.com>
2026-09-24 10:17:10 +0530

    Time: 2 ms (95.28%), Space: 43.4 MB (66.48%) - LeetHub

---
+++ 0682-baseball-game/0682-baseball-game.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int calPoints(String[] operations) {
+        Stack<Integer> stack= new Stack<>();
+        for (String op : operations){
+            if (op.equals("C")){
+                stack.pop();
+
+            }
+            else if (op.equals("D")){
+                int last = stack.peek();
+                stack.push(last*2);
+
+            }
+            else if ( op.equals("+")){
+                int last = stack.pop();
+                int secondlast = stack.peek();
+
+                stack.push(last);
+                stack.push(last + secondlast);
+            }
+            else {
+                stack.push(Integer.parseInt(op));
+
+            }
+        }
+        int sum = 0;
+        while (!stack. isEmpty()){
+            sum += stack.pop();
+        }
+        return sum;
+        
+    }
+}
